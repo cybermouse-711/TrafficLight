c496096ae32c36ec3b43096b0cf20788e6d047f4
ChauveSouris <desseobscurite@gmail.com>
2023-08-22 22:53:18 +0300

    Created fail for Circle

---
+++ TrafficLight/TrafficLightCircle.swift
@@ -0,0 +1,26 @@
+//
+//  TrafficLightCircle.swift
+//  TrafficLight
+//
+//  Created by Елизавета Медведева on 22.08.2023.
+//
+
+import SwiftUI
+
+struct TrafficLightCircle: View {
+    let color: Color
+    
+    var body: some View {
+        Circle()
+            .foregroundColor(color)
+            .frame(width: 150)
+            .overlay(Circle().stroke(Color.white, lineWidth: 3))
+            .shadow(radius: 10)
+    }
+}
+
+struct TrafficLightCircle_Previews: PreviewProvider {
+    static var previews: some View {
+        TrafficLightCircle(color: .gray)
+    }
+}
