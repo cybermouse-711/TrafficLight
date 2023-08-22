111c55e76c724d155d030379fc41e65b94e0342e
ChauveSouris <desseobscurite@gmail.com>
2023-08-22 22:41:57 +0300

    Initial Commit

---
+++ TrafficLight/TrafficLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightApp.swift
+//  TrafficLight
+//
+//  Created by Елизавета Медведева on 22.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
